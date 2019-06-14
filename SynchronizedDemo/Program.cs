e0af4584faa75c330728273fcc01e9ca456a58f9
landwind <517773735@qq.com>
2019-06-14 10:54:46 +0800

    添加项目文件。

---
+++ SynchronizedDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SynchronizedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //题目：利用5个线程并发执行，num数字累计计数到10000，并打印。
+            Console.WriteLine("!");
+        }
+    }
+}
